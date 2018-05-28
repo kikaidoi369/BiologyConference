fa1b914faa1b2838f814795cdcc99ed1e7da031c
xxxx <apiwat@hollywoodmovies.co.th>
2018-05-28 13:53:01 +0700

    Add register.js

--- js/register.js
+++ js/register.js
@@ -146,6 +146,43 @@ function checkPasswordMatch()
     }
 }
 
+$(function() {
+  google.script.run.withSuccessHandler(showThings)
+      .getMenuListFromSheet();
+  google.script.run.withSuccessHandler(showMenu)
+      .getMenuListFromSheet();
+});
+
+function showThings(things) {
+  var list = $('#country');
+  list.empty();
+  for (var i = 0; i < things.length; i++) {
+    list.append('<li>' + things[i] + '</li>');
+  }
+}
+
+function showMenu(menuItems) {
+  var list = $('#menu');
+  list.find('option').remove();  // remove existing contents
+
+  for (var i = 0; i < menuItems.length; i++) {
+    list.append('<option>' + menuItems[i] + '</option>');
+  }
+}
+
+$(function() {
+  google.script.run.withSuccessHandler(showThings)
+      .getLotsOfThings();
+});
+
+function showThings(things) {
+  var list = $('#country');
+  list.empty();
+  for (var i = 0; i < things.length; i++) {
+    list.append('<li>' + things[i] + '</li>');
+  }
+}
+
 $(function(){
   
   // $(".dropdown-menu li a").click(function(){
